ecae440443b5396800667f9dd1142e2275f5baba
Anurag Yadav <150878739+ANURAGYADAV008@users.noreply.github.com>
2025-01-31 00:45:05 +0530

    Time: 28 ms (14.33%), Space: 23.4 MB (65.22%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,40 +10,18 @@
 
 class Solution {
 public:
-    bool dfs(TreeNode* root,TreeNode* p, vector<TreeNode*>&path){
-        if(root==NULL){
-            return false;
-        }
-        path.push_back(root);
-        if(root==p){
-            return true;
-        }
-        if(dfs(root->left,p,path)||dfs(root->right,p,path)){
-           return true; 
-        }
-        path.pop_back();
-        return false;
-        
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    vector<TreeNode*>path1;
-    vector<TreeNode*>path2;
-    dfs(root,p,path1);
-    dfs(root,q,path2);
-
-    TreeNode* lca=NULL;
-    int minlen = min(path1.size(),path2.size());
-    for(int i=0; i<minlen; i++){
-        if(path1[i]==path2[i]){
-            lca = path1[i];
+        if (root == NULL || root == p || root == q) {
+            return root; // Base case: found p or q or reached NULL
         }
-        else{
-            break;
+
+        TreeNode* leftLCA = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightLCA = lowestCommonAncestor(root->right, p, q);
+
+        if (leftLCA && rightLCA) {
+            return root; // If both subtrees return a non-null node, root is LCA
         }
         
-    }
-    return lca;
-
-
+        return leftLCA ? leftLCA : rightLCA; // Otherwise, return the non-null side
     }
 };
