0e6fb0fa7d28fca1fba8e20327350c9393fe3ff7
Anurag Yadav <150878739+ANURAGYADAV008@users.noreply.github.com>
2025-02-18 22:51:48 +0530

    Time: 70 ms (42.19%), Space: 109.7 MB (31.89%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    void generateall(vector<int>&v, vector<vector<int>>&ans, vector<int>&two,int i,int k){
+    void generateall(int n, vector<vector<int>>&ans, vector<int>&two,int i,int k){
     if(two.size()==k){
         ans.push_back(two);
         return;
     }
-    if(i>=v.size())return;
-    two.push_back(v[i]);
-    generateall(v,ans,two,i+1,k);
+    if(i>n)return;
+    two.push_back(i);
+    generateall(n,ans,two,i+1,k);
     two.pop_back();
-    generateall(v,ans,two,i+1,k);
+    generateall(n,ans,two,i+1,k);
     
 }
     vector<vector<int>> combine(int n, int k) {
     vector<vector<int>>ans;
-    vector<int>v(n);
-    for(int i=0;i<n; i++){
-        v[i]=i+1;
-    }
     vector<int>two;
-    generateall(v,ans,two,0,k);
+    generateall(n,ans,two,1,k);
     return ans;
       
     }
