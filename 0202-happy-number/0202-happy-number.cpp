9a1491e56c858b1b1d9ac3b9c0ba91f5ea3c8613
Anurag Yadav <150878739+ANURAGYADAV008@users.noreply.github.com>
2025-02-26 00:47:18 +0530

    Time: 2 ms (25.9%), Space: 8.5 MB (8.09%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int>seen;
+        
+    while(n!=1){
+        if(seen.find(n)!=seen.end()){
+            return false;
+        }
+        seen.insert(n);
+        int ans=0;
+        while(n>0){
+           int num=n%10;
+            ans+= num*num;
+            n=n/10;
+
+        }
+        n=ans;
+        
+    }
+    return true;
+    }
+        
+};
